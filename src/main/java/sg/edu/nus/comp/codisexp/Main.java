4404d9fc61720941e5c3d808c71bbe8546202c80
Sergey Mechtaev <mechtaev@gmail.com>
2016-04-15 16:49:04 +0800

    updating example

--- src/main/java/sg/edu/nus/comp/codisexp/Main.java
+++ src/main/java/sg/edu/nus/comp/codisexp/Main.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.comp.codisexp;
 
+import org.apache.commons.lang3.tuple.Triple;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sg.edu.nus.comp.codis.*;
 import sg.edu.nus.comp.codis.ast.*;
@@ -16,59 +18,66 @@ import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
 
-        LoggerFactory.getLogger(Main.class).info("from Main");
+        Logger logger = LoggerFactory.getLogger(Main.class);
 
-        Hole i = new Hole("i", IntType.TYPE, Node.class);
-        Hole j = new Hole("j", IntType.TYPE, Node.class);
-        Hole a = new Hole("a", BoolType.TYPE, Node.class);
-        Hole b = new Hole("b", BoolType.TYPE, Node.class);
-
+        DivergentTest generator = new DivergentTest(Z3.getInstance());
         Synthesis synthesizer = new CBS(Z3.getInstance());
 
-        Map<Node, Integer> componentMultiset = new HashMap<>();
-        ProgramVariable x = new ProgramVariable("x", IntType.TYPE);
-        ProgramVariable y = new ProgramVariable("y", IntType.TYPE);
-        componentMultiset.put(x, 2);
-        componentMultiset.put(y, 2);
-        componentMultiset.put(new Greater(i, j), 2);
-        componentMultiset.put(new And(a, b), 1);
-        componentMultiset.put(new Or(a, b), 1);
-        componentMultiset.put(new Parameter("alpha", IntType.TYPE), 1);
-        componentMultiset.put(new Parameter("beta", IntType.TYPE), 1);
+        // correct repair is (x - y < 2)
 
+        Map<Node, Integer> componentMultiset = new HashMap<>();
+        ProgramVariable x = ProgramVariable.mkInt("x");
+        ProgramVariable y = ProgramVariable.mkInt("y");
+        Parameter p = Parameter.mkInt("p");
+        componentMultiset.put(x,1);
+        componentMultiset.put(y,1);
+        componentMultiset.put(p, 1);
+        componentMultiset.put(Components.ADD,1);
+        componentMultiset.put(Components.SUB, 1);
+        componentMultiset.put(Components.GT,1);
+        componentMultiset.put(Components.GE, 1);
 
         ArrayList<TestCase> testSuite = new ArrayList<>();
         Map<ProgramVariable, Node> assignment1 = new HashMap<>();
-        assignment1.put(x, IntConst.of(2));
-        assignment1.put(y, IntConst.of(1));
+        assignment1.put(x,IntConst.of(2));
+        assignment1.put(y,IntConst.of(1));
         testSuite.add(new TestCase(assignment1, BoolConst.TRUE));
 
         Map<ProgramVariable, Node> assignment2 = new HashMap<>();
         assignment2.put(x, IntConst.of(1));
-        assignment2.put(y, IntConst.of(3));
-        testSuite.add(new TestCase(assignment2, BoolConst.FALSE));
+        assignment2.put(y, IntConst.of(4));
+        testSuite.add(new TestCase(assignment2, BoolConst.TRUE));
 
         Map<ProgramVariable, Node> assignment3 = new HashMap<>();
-        assignment3.put(x, IntConst.of(2));
-        assignment3.put(y, IntConst.of(0));
+        assignment3.put(x, IntConst.of(4));
+        assignment3.put(y, IntConst.of(1));
         testSuite.add(new TestCase(assignment3, BoolConst.FALSE));
 
         Map<ProgramVariable, Node> assignment4 = new HashMap<>();
-        assignment4.put(x, IntConst.of(0));
+        assignment4.put(x, IntConst.of(1));
         assignment4.put(y, IntConst.of(1));
-        testSuite.add(new TestCase(assignment4, BoolConst.FALSE));
+        testSuite.add(new TestCase(assignment4, BoolConst.TRUE));
 
-        Map<ProgramVariable, Node> assignment5 = new HashMap<>();
-        assignment5.put(x, IntConst.of(0));
-        assignment5.put(y, IntConst.of(0));
-        testSuite.add(new TestCase(assignment5, BoolConst.FALSE));
+        logger.info("Want to generate (x - y < 2) but don't have enough tests");
 
         Optional<Node> node = synthesizer.synthesize(testSuite, componentMultiset);
-        if (node.isPresent()) {
-            System.out.println("Original: " + node.get());
-            System.out.println("Simplified: " + Simplifier.symplify(node.get()));
+        if(node.isPresent()) {
+            logger.info("Synthesized patch: " + node.get());
+            logger.info("Simplified: "
+                    + Simplifier.symplify(node.get()));
         } else {
-            System.out.println("FAIL");
+            logger.info("FAIL");
         }
+
+        logger.info("Generating additional test ...");
+
+        Optional<Triple<TestCase, Node, Node>> result = generator.generate(componentMultiset, testSuite);
+
+        if (result.isPresent()) {
+            logger.info("Divergent test: " + result.get().getLeft());
+            logger.info("First program: " + result.get().getMiddle());
+            logger.info("Second program: " + result.get().getRight());
+        }
+
     }
 }
